791b82739dd665f3523fc786d98126cea0380e10
Rafael <75639189+sdeoliveira-r@users.noreply.github.com>
2022-08-31 19:18:01 -0300

    Update curso.service.ts
    Update baseURL

--- src/app/curso/curso.service.ts
+++ src/app/curso/curso.service.ts
@@ -13,7 +13,7 @@ import { Curso } from './curso';
 export class CursoService {
 
   //URL
-  url = "https://sdeoliveira-r.github.io/projeto-api/scripts-php-database/";
+  url = "https://github.com/sdeoliveira-r/projeto-api/scripts-php-database/";
 
   //Vetor
   vetor: Curso[] = [];
